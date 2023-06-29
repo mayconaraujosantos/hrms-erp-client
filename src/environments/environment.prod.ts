9ff780950005f272bf2a2e7af8f5ccfef1fe48e0
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-06-29 14:24:57 -0300

    chore: add config pre-commit and commit-msg hooks

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  urlBase: `http://${location.hostname}:8080`,
+  urlSocket: `ws://${location.hostname}:8080`
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  urlBase: `http://${location.hostname}:8080`,
+  urlSocket: `ws://${location.hostname}:8080`
+}
