2d20094d92fd3cb4e553485c0dc1c97cc8c9a490
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-06-29 20:01:19 -0300

    chore: add enviroment config prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  urlBase: `http://${location.hostname}:8080`,
-  urlSocket: `ws://${location.hostname}:8080`,
+  urlBase: `https://hrms-erp-production.up.railway.app`,
 };
