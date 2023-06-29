d527b2d513e0f406789506a77d71502534e2b5d0
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-06-29 16:20:16 -0300

    chore: add jest config

--- src/app/app.material-module.ts
+++ src/app/app.material-module.ts
@@ -1,2 +1,1 @@
-export class
-AppMaterialModule {}
+export class AppMaterialModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlBase: `http://${location.hostname}:8080`,
-  urlSocket: `ws://${location.hostname}:8080`
-}
+  urlSocket: `ws://${location.hostname}:8080`,
+};
